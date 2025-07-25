652c08afc8208491d4d1784a4cee9a191a98b419
s30920 <s30920@pjwstk.edu.pl>
2025-07-25 21:00:33 +0300

    +nuget package

--- src/DAL/Data/CryptoContext.cs
+++ src/DAL/Data/CryptoContext.cs
@@ -24,9 +24,9 @@ public partial class CryptoContext : DbContext
 
     public virtual DbSet<FetchSchedule> FetchSchedules { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1436;User Id=sa;Password=2f$A_Xs9;Trust Server Certificate=True");
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        // => optionsBuilder.UseSqlServer("Server=localhost,1436;User Id=sa;Password=2f$A_Xs9;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
